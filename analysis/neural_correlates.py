cbdd8d418320820d6131c21e84ac1daa89a5f5e1
David Ungarish <david.unga@gmail.com>
2024-04-11 18:38:41 +0300

    neural correlates - generalize weight extraction

--- analysis/neural_correlates.py
+++ analysis/neural_correlates.py
@@ -2,25 +2,29 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import numpy as np
-from common.utils import dlutils
-from config import Config
+import torch.nn
 from data_manager import DataMgr
 from common.utils.typings import *
-from pathlib import Path
-from common.metric_learning.embedding_models import LinearEmbedder
-from common.metric_learning import embedding_eval
 import cv_results_mgr
 from common.utils.inlier_detection import InlierDetector
 
 
-def extract_model_weights(model: LinearEmbedder) -> NpMatrix:
-    return model.embedder.get_submodule('1').weight.detach().numpy()
+def extract_model_weights(model: torch.nn.Module) -> NpMatrix:
+    names = [name for name in model.state_dict() if 'weight' in name]
+    assert len(names) == 1
+    weights = model.state_dict()[names[0]].detach().numpy()
+    return weights
 
 
-def identify_subpopulation_by_model_weights(model_file, draw = True):
+def identify_subpopulation_by_model_weights(model_file, draw=True):
+    """
+    Estimate the contribution of each input neuron to the embedding model,
+    and split the neural population into subpopulations according to their contribution scores.
+    optionally draw the result.
+    """
 
     # ----
-    neuron_weight_agg = 'median'
+    neuron_weight_agg = 'median'  # aggregation of weight scores per neuron
     inlier_detector = InlierDetector(method='iqr')
     # ----
 
@@ -38,16 +42,22 @@ def identify_subpopulation_by_model_weights(model_file, draw = True):
         w = weights[:, neuron_name_per_weight_col == neuron_name]
         neuron_weight[i] = neuron_weight_agg_func(np.abs(w))
 
-    inliers_mask = ~inlier_detector.fit_predict(neuron_weight)
+    outlrs_mask = ~inlier_detector.fit_predict(neuron_weight)
+
+    assert neuron_weight[outlrs_mask].min() > neuron_weight[~outlrs_mask].max(), \
+        "Minority population score is not strictly greater than majority's"
+
     df = pd.DataFrame({'Neuron Weight': neuron_weight, 'Neuron Name': neuron_names,
-                       'Neuron Index': np.arange(len(neuron_names)), 'Population': inliers_mask.astype(int)})
+                       'Neuron Index': np.arange(len(neuron_names)), 'Population': outlrs_mask.astype(int)})
 
     if draw:
+        df['Population'] = ['Main' if p == 0 else 'Sub' for p in df['Population']]
         jp = sns.jointplot(data=df, x='Neuron Index', y='Neuron Weight', hue='Population', kind='scatter',
-                           palette={0: 'black', 1: 'red'}, alpha=.75)
+                           palette={'Main': 'black', 'Sub': 'red'}, alpha=.75)
         jp.ax_marg_x.remove()
         xlm_margin = 2
         jp.ax_joint.set_xlim([-xlm_margin, len(neuron_names) + xlm_margin - 1])
+        plt.title("Contribution of Neurons to Embedded Representation" + "\n")
         plt.show()
 
     return df
