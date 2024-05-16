143cfa568acb741e3022a00c6f6f9d60e56babb6
David Ung <david.unga@gmail.com>
2024-05-16 13:54:23 +0300

    add gpu runner

---
+++ run-gpu-01.sh
@@ -0,0 +1,17 @@
+module=Python/3.10.4-GCCcore-11.3.0
+pyfile=analysis/cv_train.py
+
+#BSUB -q gpu-medium
+#BSUB -J geometric-neurons
+#BSUB -o out.%J
+#BSUB -e err.%J
+#BSUB -R "rusage[mem=4096]"
+#BSUB -n 8
+#BSUB -W 24:00
+
+module load $module
+cd ~/geometric-neurons
+source bin/activate
+git pull
+export PYTHONPATH='.'
+python $pyfile
