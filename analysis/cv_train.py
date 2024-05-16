99e6be1f3cecbe5dd20209dff682aa632f4ac47b
David Ung <david.unga@gmail.com>
2024-05-16 12:55:16 +0300

    expand params grid

--- analysis/cv_train.py
+++ analysis/cv_train.py
@@ -196,4 +196,4 @@ def run_cv(exists_handling: Literal["warm_start", "overwrite", "skip", "error"] 
 
 if __name__ == "__main__":
     cv_results_mgr.refresh_results_file()
-    run_cv(exists_handling="overwrite", dbg_run=False, early_stop_epoch=20)
+    run_cv(exists_handling="overwrite", dbg_run=False, early_stop_epoch=None)
